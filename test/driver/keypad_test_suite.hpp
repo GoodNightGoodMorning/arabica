c583fb61c9dca32700a5643c63e057240440967d
gapry <gapry@protonmail.com>
2024-06-12 03:14:14 +0800

    keypad_test_suite: rename macros

--- test/driver/keypad_test_suite.hpp
+++ test/driver/keypad_test_suite.hpp
@@ -3,15 +3,15 @@
 #include <arabica/emulator/emulator.hpp>
 #include <gtest/gtest.h>
 
-#define arabica_onekey_press_test(test_case_name, test_case_body) \
-  TEST(keypad_test_suite, test_case_name) {                       \
-    arabica::Emulator emulator;                                   \
-    test_case_body                                                \
+#define arabica_keypress_test_impl(test_case_name, test_case_body) \
+  TEST(keypad_test_suite, test_case_name) {                        \
+    arabica::Emulator emulator;                                    \
+    test_case_body                                                 \
   }
 // ----
 
-#define arabica_allkey_press_test(i)                    \
-  arabica_onekey_press_test(test_key##i##_down, {       \
+#define arabica_keypress_test(i)                        \
+  arabica_keypress_test_impl(test_key##i##_down, {      \
     emulator.keypad.keydown_code = 0x##i;               \
     emulator.memory.write(emulator.cpu.pc, 0xF0);       \
     emulator.memory.write(emulator.cpu.pc + 1, 0x0A);   \
@@ -20,19 +20,19 @@
   });                                                   \
   // ----
 
-arabica_allkey_press_test(0);
-arabica_allkey_press_test(1);
-arabica_allkey_press_test(2);
-arabica_allkey_press_test(3);
-arabica_allkey_press_test(4);
-arabica_allkey_press_test(5);
-arabica_allkey_press_test(6);
-arabica_allkey_press_test(7);
-arabica_allkey_press_test(8);
-arabica_allkey_press_test(9);
-arabica_allkey_press_test(a);
-arabica_allkey_press_test(b);
-arabica_allkey_press_test(c);
-arabica_allkey_press_test(d);
-arabica_allkey_press_test(e);
-arabica_allkey_press_test(f);
+arabica_keypress_test(0);
+arabica_keypress_test(1);
+arabica_keypress_test(2);
+arabica_keypress_test(3);
+arabica_keypress_test(4);
+arabica_keypress_test(5);
+arabica_keypress_test(6);
+arabica_keypress_test(7);
+arabica_keypress_test(8);
+arabica_keypress_test(9);
+arabica_keypress_test(a);
+arabica_keypress_test(b);
+arabica_keypress_test(c);
+arabica_keypress_test(d);
+arabica_keypress_test(e);
+arabica_keypress_test(f);
