5e21227210f7b39b43fb5a76238542657e2fdb19
gapry <gapry@protonmail.com>
2024-07-11 21:57:10 +0800

    memory_test_suite: remove the redundant function write_and_assert

--- test/memory/memory_test_suite.hpp
+++ test/memory/memory_test_suite.hpp
@@ -6,14 +6,9 @@
 #define arabica_memory_test(test_case, address, value) \
   TEST(memory_test_suite, test_case) {                 \
     arabica::Emulator emulator;                        \
-    write_and_assert(address, value);                  \
+    emulator.memory.write(address, value);             \
+    ASSERT_EQ(emulator.memory.read(address), value);   \
   }
-
-void write_and_assert(const arabica::Memory::address_t address, const arabica::Memory::value_t value) {
-  arabica::Memory memory;
-  memory.write(address, value);
-  ASSERT_EQ(memory.read(address), value);
-}
 
 arabica_memory_test(pc_write_001, emulator.cpu.pc, 0x61);
 arabica_memory_test(pc_write_002, emulator.cpu.pc, 0x00);
